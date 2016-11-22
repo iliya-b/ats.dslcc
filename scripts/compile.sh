779bf3b463d837672467c6ed67ece6b07c315dca
Marco Mariani <marco.mariani@alterway.fr>
2016-11-22 18:26:52 +0100

    script error when compilation fails

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# Any error must exit the script with a status code
 set -e
 
 cat /dev/fd/0 > /home/developer/com.zenika.aicdsl/DslFiles/test.aic
+
+# XXX this never fails, check later
 gradle generateJavaFile
-[ -f /home/developer/com.zenika.aicdsl/DslFiles/Testing.java ] || exit 10
+
+# Did it create a file?
+if [ ! -f /home/developer/com.zenika.aicdsl/DslFiles/Testing.java ]; then
+   echo "ERROR: No Java file was generated"
+   exit 10
+fi
+
+# Is there any test ?
+if ! grep -q @Test /home/developer/com.zenika.aicdsl/DslFiles/Testing.java; then
+	echo "ERROR: No @Test methods was generated"
+	exit 20
+fi
 
